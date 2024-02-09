71da5f696248b90c8a37a4a8315cddbf6326e683
Ankitsingh Bisht <ankitsingh.bisht@indxtechnology.com>
2024-02-10 00:35:41 +0530

    Add task template

--- app.js
+++ app.js
@@ -6,19 +6,18 @@ const isValidDate = (currDate, selectedDate) => {
     const selected = new Date(selectedDate);
     return selected >= currentDate;
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("task-deadline").value = getCurrentDate();
-    document.getElementById("task-deadline").min = getCurrentDate() ;
-  });
-document.getElementById("task-input-box").addEventListener("keydown", function(event) {
+    document.getElementById("task-deadline").min = getCurrentDate();
+});
+document.getElementById("task-input-box").addEventListener("keydown", function (event) {
     const text = document.getElementById("task-input-box").value?.trim();
     const selectedDate = document.getElementById("task-deadline").value;
     const currentDate = getCurrentDate();
     const isValidDateToBeAdded = isValidDate(currentDate, selectedDate);
     if (event.key === "Enter" && text && text.length > 0 && isValidDateToBeAdded) {
-      console.log(text);
-      console.log(selectedDate)
-      // You can perform additional actions here if needed
+        console.log(text);
+        console.log(selectedDate)
+        // You can perform additional actions here if needed
     }
-  });
-  
+});
